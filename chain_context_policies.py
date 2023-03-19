da3665715f9e321fc9c6109b1fb88ce9ea4b2543
while0x1 <94356664+while0x1@users.noreply.github.com>
2023-03-20 00:49:34 +0800

    Update chain_context_policies.py

--- chain_context_policies.py
+++ chain_context_policies.py
@@ -40,5 +40,36 @@ for n in assetDict:
 					print(policy_hex,asset_name,asset_amount)
 					index += 1
 
+#Find policy in Utxo List
+find_policy = '8eb7927db11995297cd8846bda6884020d4c2dd9d8886e456921465b'
+find_name = b'VeriFair_alpha_Winners'
+find_nft = MultiAsset.from_primitive({bytes.fromhex(find_policy): {find_name: 1} })
+
+find_policy = '366a7e727f403128a0c87965b23aaa95965c22dfcb6f9f41318e8412'
+find_name = b'Cardano Circles'
+find_nft = MultiAsset.from_primitive({bytes.fromhex(find_policy): {find_name: 1} })
+
+
+for utxo in utxos:
+	if utxo.output.amount.multi_asset:
+		if len(utxo.output.amount.multi_asset) < 2:
+			if utxo.output.amount.multi_asset == find_nft:
+				print('Gotchya')
+		else:
+			for items in utxo.output.amount.multi_asset:
+				policy = items.to_primitive().hex()
+				if len(utxo.output.amount.multi_asset[items]) < 2:
+					asset_name = list(utxo.output.amount.multi_asset[items].to_primitive())[0]
+					print(policy,asset_name)
+					if find_name == asset_name and find_policy == policy:
+						print('Gotchya Here!')
+				else:
+					for names in utxo.output.amount.multi_asset[items]:
+						asset_name = names.to_primitive()
+						x = bytes(asset_name.decode('utf-8'), 'utf-8')
+						if find_name == x and find_policy == policy:
+							print('Gotchya Here!!!')
+						print(policy,x)
+						
 			
     
