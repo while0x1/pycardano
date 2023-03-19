ec29ebfce8ac2b9c49d551a254c06840674c605e
while0x1 <94356664+while0x1@users.noreply.github.com>
2023-03-19 23:20:39 +0800

    Update chain_context_policies.py

--- chain_context_policies.py
+++ chain_context_policies.py
@@ -17,13 +17,28 @@ for n in assetDict:
 	for q in n:
 		policies.append(q.to_primitive().hex())
 		
+index  = 0 
 for n in assetDict:
-	if len(n) < 2:
-		keys = list(n)
+	keys = list(n)
+	if len(keys) < 2:
 		policy_hex = keys[0].to_primitive().hex()
 		asset_name = list(n[keys[0]].to_primitive())[0]
 		asset_amount = list(n[keys[0]].to_primitive().values())[0]
 		print(policy_hex,asset_name,asset_amount)
+	else:
+		for items in n:
+			if len(n[items]) < 2:
+				policy_hex = items.to_primitive().hex()
+				asset_name = list(n[items].to_primitive())[0]
+				asset_amount = list(n[items].to_primitive().values())[0]
+				print(policy_hex,asset_name,asset_amount)
+			else:
+				for names in n[items]:
+					policy_hex = items.to_primitive().hex()
+					asset_name = bytes(names)
+					asset_amount = list(n[items].values())[index]
+					print(policy_hex,asset_name,asset_amount)
+					index += 1
 
 			
     
